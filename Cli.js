b7aaf263c3c006c0b08509fbf081032a90e10ab6
HommePretentieux <h05p1tal13r.leon@gmail.com>
2023-12-08 01:18:53 +0100

    error correction on cli.js

--- Cli.js
+++ Cli.js
@@ -1,13 +1,8 @@
 const Teacher = require('./teacher');
 const GiftParser = require('./parser/giftParser');
-const question = require('./parser/Question')
-
 const { program } = require('@caporal/core');
 const prompt = require('prompt-sync')();
-const fs= require('fs');
-const Question = require('./parser/Question');
-
-
+const fs = require('fs').promises; // Use promises version of fs
 
 program
   .command('createTest', 'create a test by asking for teacher information and questions')
@@ -17,96 +12,147 @@ program
 
     teacher.generateVCard();
     console.log('vCard created successfully.');
-    saveToFile('', `./Test/${teacher.fullName}/${testname}.gift`);
-    console.log(`Test created successfully.`);
+
+    try {
+      await saveToFile('', `./Test/${teacher.fullName}/${testname}.gift`);
+      console.log(`Test created successfully.`);
+    } catch (error) {
+      console.error('Error creating test:', error.message);
+    }
   })
 
   .command('addQuestion', 'add a question to a test')
   .action(async () => {
-    const teachername = prompt('teacher\'s name : ').replace(/\s/g, '_')
+    const teachername = prompt('teacher\'s name : ').replace(/\s/g, '_');
     const testname = prompt('test name : ').replace(/\s/g, '_');
-    const question = await getQuestionsFromTeacher();
-    const giftFormat = convertToGiftFormat(question);
 
-    fs.appendFile(`./Test/${teachername}/${testname}.gift`, giftFormat, function (err) {   if (err) throw err;   console.log('Fichier créé !');});
-    console.log(`question added successfully.`);
-  })
-
-  .command('verfyTest', 'verify if a test has between 15 and 20 questions')
-  .action(async () => {
-    const teachername = prompt('teacher\'s name')
-    const testname = prompt('test name:').replace(/\s/g, '_');
-   // count question in gift file 
-   //print if less / print if good / print if more
-  })
-  .command('loadQB', 'load question bank from a gift file')
-  .argument('<file>', 'The file containing questino to add to the question bank')
-  .action(async ({args, option, logger}) => {
-    fs.mkdir('./questionBank', (err) => {
-      if (err) {
-      console.error(err);
-      } else {
-      // Création réussie !
+    try {
+      const question = await getQuestionsFromTeacher();
+      const giftFormat = question.gift_format;
+      const data= await parseData(`./Test/${teachername}/${testname}.gift`);
+      if (data.length>=20){
+        console.log("file contains 20 questions already. You can't add question anymore")
       }
-      fs.appendFile(`./questionBank/questionBank.gift`, args.file, function (err) {   if (err) throw err;   console.log('Fichier créé !');});
-    })
-  // check Vpf
-	.command('check', 'Check if <file> is a valid gift file')
-	.argument('<file>', 'The file to check with gift parser')
-	.option('-s, --showSymbols', 'log the analyzed symbol at each step', { validator : cli.BOOLEAN, default: false })
-	.option('-t, --showTokenize', 'log the tokenization results', { validator: cli.BOOLEAN, default: false })
-	.action(({args, options, logger}) => {
-		
-		fs.readFile(args.file, 'utf8', function (err,data) {
-			if (err) {
-				return logger.warn(err);
-			}
-	  
-			var analyzer = new VpfParser(options.showTokenize, options.showSymbols);
-			analyzer.parse(data);
-			
-			if(analyzer.errorCount === 0){
-				logger.info("The .gift file is a valid gift file".green);
-			}else{
-				logger.info("The .gift file contains error".red);
-			}
-			
-			logger.debug(analyzer.parsedPOI);
+      else{
+       await fs.appendFile(`./Test/${teachername}/${testname}.gift`, giftFormat);
+       console.log(`Question added successfully.`); 
+      }
+      
+    } catch (error) {
+      console.error('Error adding question:', error.message);
+    }
+  })
 
-		});
-   // count question in gift file 
-   //print if less / print if good / print if more
+  .command('read', 'reads the questionBank')
+  .action(async () => {
+    try {
+      const data= await parseData(`./questionBank/questionBank.gift`);
+      for(let i=0;i<data.length;i++){
+        console.log("question "+i+" : ");
+        data[i].visualise();
+        console.log('\n \n');
+      }
+    } catch (error) {
+      console.error('Error :', error.message);
+    }
+  })
+
+  .command('finish', 'verify if a test has between 15 and 20 questions')
+  .action(async () => {
+    const teachername = prompt('teacher\'s name : ').replace(/\s/g, '_');
+    const testname = prompt('test name :').replace(/\s/g, '_');
+
+    try {
+      const data = await parseData(`./Test/${teachername}/${testname}.gift`);
+      let isEveryQuestionUnique=true;
+      for(let i=0;i<data.length;i++){
+        for (let j=i+1;j<data.length;j++){
+          if (data[i].title==data[j].title){
+            isEveryQuestionUnique=false;
+            data.splice(j, 1); 
+            j--;
+          }
+        }
+      }
+      if(!isEveryQuestionUnique){
+        fs.unlink(`./Test/${teachername}/${testname}.gift`);
+        for(let i=0;i<data.length;i++){
+          fs.appendFile(`./Test/${teachername}/${testname}.gift`, data[i].gift_format);
+        }
+        console.log("multiple identic questions. file corrected. please retry");
+      }
+      else if (data.length>=15&&data.length<=20&&isEveryQuestionUnique){
+        await fs.rename(`./Test/${teachername}/${testname}.gift`, `./TestBank/${teachername}/${testname}.gift`)
+      }
+      else{console.log(`this test has ${data.length} questions, a test has to have between 15 and 20 questions`);} 
+     
+    } catch (error) {
+      console.error('Error :', error.message);
+      return null;
+    }
+  })
+
+  .command('loadQB', 'Load questions from a GIFT file into the question bank')
+  .argument('<file>', 'The file containing questions to add to the question bank')
+  .action(async ({ args, logger }) => {
+    try {
+      await fs.mkdir('./questionBank', { recursive: true });
+      const data = await fs.readFile(args.file, 'utf8');
+      await fs.appendFile('./questionBank/questionBank.gift', data);
+      console.log('File appended successfully.');
+    } catch (error) {
+      console.error('Error loading questions:', error.message);
+    }
   });
 
 async function getTeacherInfo() {
-
   console.log('Enter teacher information:');
   const name = prompt('Name: ');
   const course = prompt('Teaching course: ');
   const phoneNumber = prompt('Phone number: ');
   const email = prompt('Email address: ');
   const address = prompt('Address: ');
-  const teacher=new Teacher(name, course, phoneNumber, email, address);
-
-  return teacher;
+  return new Teacher(name, course, phoneNumber, email, address);
 }
 
 async function getQuestionsFromTeacher() {
-    console.log('Enter questions for the test:');
-    const qname = prompt(`Question : `);
-    const question = Question.findQuestion(qname);
+  try {
+    const data = await parseData('./questionBank/questionBank.gift');
+    console.log('Enter question index for the test:');
+    const qindex = parseInt(prompt(`Question index: `));
 
-  return question;
+    const question = data[qindex]; //.find((q) => q.title === qname);
+    if (question) {
+      console.log("Question found:");
+      console.log(question);
+      return question;
+    } else {
+      console.log('Question not found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error getting questions:', error.message);
+    return null;
+  }
 }
 
-function convertToGiftFormat(questions) {
-  const giftFormat = questions.map((question, index) => `::Question${index + 1}:: ${question}`).join('\n');
-  return giftFormat;
+
+async function saveToFile(data, filename) {
+  await fs.writeFile(filename, data);
 }
 
-function saveToFile(data, filename) {
-  fs.writeFileSync(filename, data);
+async function parseData(filePath) {
+  const data = await fs.readFile(filePath, 'utf8');
+  let analyzer = new GiftParser(false, false);
+  analyzer.parse(data);
+
+  if (analyzer.errorCount === 0) {
+    console.log("The .gift file is a valid gift file");
+  } else {
+    console.log("The .gift file contains error");
+  }
+
+  return analyzer.parsedQuestion;
 }
 
 program.run();
-})
